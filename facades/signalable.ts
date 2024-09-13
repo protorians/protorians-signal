8f16ec7a81fb591fe2dad033a056fa77fd360c88
Protorians <protorians.dev@gmail.com>
2024-09-13 02:03:38 +0000

    Fix : exports mode

--- facades/signalable.ts
+++ facades/signalable.ts
@@ -1,6 +1,9 @@
 import {Signalables} from "../supports";
 
-
+/**
+ * Create new Signalable instance
+ * @param instance
+ */
 export function useSignalable<IInstance, IBlueprint>(instance: IInstance) {
 
   return new Signalables<IInstance, IBlueprint>(instance);
